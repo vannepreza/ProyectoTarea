8f545472bca725e9845595b77df7cdb8783e0766
vanne <ph12010@ues.edu.sv>
2019-05-30 15:01:33 -0600

    verificacion de control

--- app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/ActividadActualizarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/ActividadActualizarActivity.java
@@ -1,7 +1,7 @@
 package sv.edu.ues.fia.eisi.proyectotarea;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,37 +13,34 @@ public class ActividadActualizarActivity extends Activity {
     ControlDBProyecto helper;
     EditText editCiclo;
     EditText editDetalle;
-    EditText editActividad;
 
-    /**
-     * Called when the activity is first created.
-     */
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_actividad_actualizar);
+        setContentView(R.layout.activity_actividad_consultar);
         helper = new ControlDBProyecto(this);
         editCiclo = (EditText) findViewById(R.id.editCiclo);
         editDetalle = (EditText) findViewById(R.id.editDetalle);
-        editActividad = (EditText) findViewById(R.id.editActividad);
     }
-
-    public void actualizarActividad(View v) {
-        Actividad actividad = new Actividad();
-        actividad.setCiclo(editCiclo.getText().toString());
-        actividad.setDetalle(editDetalle.getText().toString());
-
-
+    public void consultarActividad(View v){
         helper.abrir();
-        String estado = helper.actualizar(actividad);
-        helper.cerrar();
+        Actividad actividad = helper.consultarActividad(editCiclo.getText().toString());
 
-        Toast.makeText(this, estado, Toast.LENGTH_SHORT).show();
+        helper.cerrar();
+        if (actividad == null)
+            Toast.makeText(this,"Actividad con ciclo " + editCiclo.getText().toString() + " no encontrado", Toast.LENGTH_LONG).show();
+        else {
+            editCiclo.setText(((Actividad) actividad).getCiclo());
+            editDetalle.setText(actividad.getDetalle());
+        }
     }
 
-    public void limpiarTexto(View v) {
+    public void limpiarTexto(View v){
+
         editCiclo.setText("");
         editDetalle.setText("");
     }
-}
 
+
+}
--- app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/baseDatos/ControlDBProyecto.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/baseDatos/ControlDBProyecto.java
@@ -11,31 +11,29 @@ import sv.edu.ues.fia.eisi.proyectotarea.modelos.Actividad;
 import sv.edu.ues.fia.eisi.proyectotarea.modelos.Ciclo;
 import sv.edu.ues.fia.eisi.proyectotarea.modelos.HorarioNo;
 
-public class ControlDBProyecto {
-    private static final String[]camposActividad = new String []
-            {"ciclo","detalle","actividad"};
-    private static final String[]camposCiclo = new String []
-            {"ciclo","detalle"};
-    private static final String[] camposHorarioNo = new String []
-            {"ciclo","fecha"};
 
-    private static final String[] camposDocente = new String []{"idDocente","nombre", "apellido", "dui", "genero", "email"};
-    private static final String[] camposCargo = new String []{"idCargo","tipoDocente", "actividadDescribir"};
-    private static final String[] camposSolicitud = new String []{"idSolicitud","descripcion", "capacidad", "fecha"};
-    private static final String[] camposCargaDocente = new String []{"idCargaDocente","nombreCarga", "idAsignaturaPesum", "idCiclo", "idSolcicitud",  "idDispoCiclo"};
+public class ControlDBProyecto {
+    private static final String[] camposActividad = new String[]{"ciclo", "detalle", "actividad"};
+    private static final String[] camposCiclo = new String[]{"ciclo", "detalle"};
+    private static final String[] camposHorarioNo = new String[]{"ciclo", "fecha"};
+    private static final String[] camposDocente = new String[]{"idDocente", "nombre", "apellido", "dui", "genero", "email"};
+    private static final String[] camposCargo = new String[]{"idCargo", "tipoDocente", "actividadDescribir"};
+    private static final String[] camposSolicitud = new String[]{"idSolicitud", "descripcion", "capacidad", "fecha"};
+    private static final String[] camposCargaDocente = new String[]{"idCargaDocente", "nombreCarga", "idAsignaturaPesum", "idCiclo", "idSolcicitud", "idDispoCiclo"};
 
 
     private final Context context;
     private DatabaseHelper DBHelper;
     private SQLiteDatabase db;
-    private int[] VMciclo;
+    //private int[] VMciclo;
 
     public ControlDBProyecto(Context ctx) {
         this.context = ctx;
         DBHelper = new DatabaseHelper(context);
     }
-    private static class DatabaseHelper extends SQLiteOpenHelper{
-        private static final String BASE_DATOS ="grupo08.s3db";
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        private static final String BASE_DATOS = "grupo08.s3db";
         private static final int VERSION = 1;
 
         public DatabaseHelper(Context context) {
@@ -44,7 +42,7 @@ public class ControlDBProyecto {
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-            try{
+            try {
                 db.execSQL("CREATE TABLE actividad(ciclo VARCHAR(7) NOT NULL PRIMARY KEY,detalle VARCHAR(30),actividad VARCHAR(30));");
                 db.execSQL("CREATE TABLE ciclo(ciclo VARCHAR(6) NOT NULL PRIMARY KEY,detalle VARCHAR(30));");
                 db.execSQL("CREATE TABLE horarioNo(ciclo VARCHAR(7) NOT NULL ,fecha DATE,PRIMARY KEY(ciclo));");
@@ -52,41 +50,44 @@ public class ControlDBProyecto {
                 db.execSQL("CREATE TABLE cargo(idCargo VARCHAR(4) NOT NULL PRIMARY KEY, tipoDocente VARCHAR(25), actividadDescribir VARCHAR(30));");
                 db.execSQL("CREATE TABLE solicitud(idSolicitud VARCHAR(4) NOT NULL PRIMARY KEY, descripcion VARCHAR(30), capacidad VARCHAR(4), fecha DATE);");
                 db.execSQL("CREATE TABLE cargaDocente(idCargaDocente VARCHAR(4) NOT NULL PRIMARY KEY,  nombreCarga VARCHAR(20), idAsignaturaPesum VARCHAR(4), idCiclo VARCHAR(6), idSolcicitud VARCHAR(4), idDispoCiclo VARCHAR(4));");
-            }catch(SQLException e){
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
+
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 // TODO Auto-generated method stub
+        }
     }
-}
-    public void abrir() throws SQLException{
+
+    public void abrir() throws SQLException {
         db = DBHelper.getWritableDatabase();
         return;
     }
-    public void cerrar(){
+
+    public void cerrar() {
         DBHelper.close();
     }
+
+
 // Inicio de insertar Actividad
 
-    public String insertar(Actividad actividad){
+    public String insertar(Actividad actividad) {
 
-        String regInsertados="Registro Insertado Nº= ";
-        long contador=0;
+        String regInsertados = "Registro Insertado Nº= ";
+        long contador = 0;
         ContentValues act = new ContentValues();
         act.put("ciclo", actividad.getCiclo());
         act.put("detalle", actividad.getDetalle());
         act.put("actividad", actividad.getActividad());
 
-        contador =db.insert("actividad", null, act);
+        contador = db.insert("actividad", null, act);
 
-        if(contador==-1 || contador==0)
-        {
-            regInsertados= "Error al Insertar el registro, Registro Duplicado. Verificar inserción";
-        }
-        else {
-            regInsertados=regInsertados+contador;
+        if (contador == -1 || contador == 0) {
+            regInsertados = "Error al Insertar el registro, Registro Duplicado. Verificar inserción";
+        } else {
+            regInsertados = regInsertados + contador;
         }
         return regInsertados;
     }
@@ -95,21 +96,18 @@ public class ControlDBProyecto {
 
     // Inicio de insertar Ciclo
 
-    public String insertar(Ciclo ciclo){
-        String regInsertados="Registro Insertado Nº= ";
-        long contador=0;
-        if(verificarIntegridad(ciclo,1))
-        {
+    public String insertar(Ciclo ciclo) {
+        String regInsertados = "Registro Insertado Nº= ";
+        long contador = 0;
+        if (verificarIntegridad(ciclo, 1)) {
             ContentValues ciclos = new ContentValues();
             ciclos.put("detalle", ciclo.getDetalle());
-            contador=db.insert("ciclo", null, ciclos);
+            contador = db.insert("ciclo", null, ciclos);
         }
-        if (contador == -1|| contador == 0)
-        {
+        if (contador == -1 || contador == 0) {
             regInsertados = "Error al insertar el registro, Registro Duplicado. Verificaar la inserción";
-        }
-        else {
-            regInsertados=regInsertados+contador;
+        } else {
+            regInsertados = regInsertados + contador;
         }
         return regInsertados;
     }
@@ -118,28 +116,27 @@ public class ControlDBProyecto {
 
     // Inicio de insertar Horario No habil
 
-    public String insertar(HorarioNo horarioNo){
+    public String insertar(HorarioNo horarioNo) {
         return null;
     }
 
     // Fin de insertar  Horario no Habil
 
 
-
     // Inicio de Actualizar Actividad
 
-    public String actualizar(Actividad actividad){
+    public String actualizar(Actividad actividad) {
 
-        if (verificarIntegridad(actividad, 4)){
-            String[]id = {actividad.getCiclo()};
+        if (verificarIntegridad(actividad, 4)) {
+            String[] id = {actividad.getCiclo()};
             ContentValues cv = new ContentValues();
             cv.put("detalle", actividad.getDetalle());
             cv.put("actividad", actividad.getActividad());
             db.update("actividad", cv, "ciclo = ?", id);
             return "Registro actualizado correctamente";
-        }else {
+        } else {
 
-            return  "Registro con ciclo " + actividad.getCiclo() + " no existe";
+            return "Registro con ciclo " + actividad.getCiclo() + " no existe";
         }
 
     }
@@ -148,7 +145,7 @@ public class ControlDBProyecto {
 
     // Inicio de Actualiar Ciclo
 
-    public String actualizar(Ciclo ciclo){
+    public String actualizar(Ciclo ciclo) {
         return null;
     }
 
@@ -156,25 +153,24 @@ public class ControlDBProyecto {
 
     // Incio de Actualizar Horario No Habil
 
-    public String actualizar(HorarioNo horarioNo){
+    public String actualizar(HorarioNo horarioNo) {
         return null;
     }
 
     // Fin de Actualizar Horario No Habil
 
 
-
     // Inicio de Eliminar Actividad
 
-    public String eliminar(Actividad actividad){
+    public String eliminar(Actividad actividad) {
 
-        String regAfectados="filas afectadas= ";
-        int contador=0;
-        if (verificarIntegridad(actividad,3)) {
-            contador+=db.delete("horario", "ciclo='"+actividad.getCiclo()+"'", null);
+        String regAfectados = "filas afectadas= ";
+        int contador = 0;
+        if (verificarIntegridad(actividad, 3)) {
+            contador += db.delete("horario", "ciclo='" + actividad.getCiclo() + "'", null);
         }
-        contador+=db.delete("actividad", "ciclo='"+actividad.getCiclo()+"'", null);
-        regAfectados+=contador;
+        contador += db.delete("actividad", "ciclo='" + actividad.getCiclo() + "'", null);
+        regAfectados += contador;
         return regAfectados;
 
     }
@@ -183,7 +179,7 @@ public class ControlDBProyecto {
 
     // Inicio de Eliminar Ciclo
 
-    public String eliminar(Ciclo ciclo){
+    public String eliminar(Ciclo ciclo) {
         return null;
     }
 
@@ -191,12 +187,11 @@ public class ControlDBProyecto {
 
     //Inicio de Eliminar Horario No Habil
 
-    public String eliminar(HorarioNo horarioNo){
+    public String eliminar(HorarioNo horarioNo) {
         return null;
     }
 
     // Fin sw Eliminar Horario No Habil
-
 
 
     // Inicio de Consultar Actividad
@@ -221,7 +216,7 @@ public class ControlDBProyecto {
 
     // Inicio de Consultar Ciclo
 
-    public Ciclo consultarCiclo(String ciclo, String detalle){
+    public Ciclo consultarCiclo(String ciclo, String detalle) {
 
         return null;
     }
@@ -230,80 +225,73 @@ public class ControlDBProyecto {
 
     // Inicio de Consultar Horario No Habil
 
-    public HorarioNo consultarHorarioNo(String ciclo){
+    public HorarioNo consultarHorarioNo(String ciclo) {
         return null;
     }
 
     // Fin de Consultar Horario No Habil
 
 
-
     //Verificacion de la integridad de los datos
 
-    private boolean verificarIntegridad(Object dato, int relacion) throws SQLException{
-        switch(relacion){
-            case 1:
-            {
-//verificar que al insertar horario exista en el ciclo y
+    private boolean verificarIntegridad(Object dato, int relacion) throws SQLException {
+        switch (relacion) {
+            case 1: {
+
                 HorarioNo horarioNo = (HorarioNo) dato;
                 String[] id1 = {horarioNo.getCiclo()};
-                //String[] id2 = {horarioNo.getCodmateria()};
+//                String[] id2 = {horarioNo.getFecha()};
 //abrir();
                 Cursor cursor1 = db.query("actividad", null, "ciclo = ?", id1, null,
                         null, null);
-                Cursor cursor2 = db.query("horarioNo", null, "ciclo = ?", id1, null, null, null);
-                if(cursor1.moveToFirst() && cursor2.moveToFirst()){
+                //   Cursor cursor2 = db.query("horarioNo", null, "ciclo = ?", id2, null, null, null);
+                // if(cursor1.moveToFirst() && cursor2.moveToFirst()){
+                if (cursor1.moveToFirst()) {
 //Se encontraron datos
                     return true;
                 }
                 return false;
             }
-            case 2:
-            {
-//verificar que al modificar nota exista carnet del alumno, el codigo de materia y el ciclo
+            case 2: {
+
                 HorarioNo horarioNo1 = (HorarioNo) dato;
-                String[] ids = {horarioNo1.getCiclo(),horarioNo1.getCiclo(),
+                String[] ids = {horarioNo1.getCiclo(), horarioNo1.getCiclo(),
                         horarioNo1.getCiclo()};
                 abrir();
                 Cursor c = db.query("horarioNo", null, "ciclo = ? AND actividad = ?", ids, null, null, null);
-                if(c.moveToFirst()){
+                if (c.moveToFirst()) {
 //Se encontraron datos
                     return true;
                 }
                 return false;
             }
-            case 3:
-            {
+            case 3: {
                 Actividad actividad = (Actividad) dato;
-                Cursor c=db.query(true, "horarioNo", new String[] {
-                                "ciclo" }, "ciclo='"+actividad.getCiclo()+"'",null,
+                Cursor c = db.query(true, "horarioNo", new String[]{
+                                "ciclo"}, "ciclo='" + actividad.getCiclo() + "'", null,
                         null, null, null, null);
-                if(c.moveToFirst())
+                if (c.moveToFirst())
                     return true;
                 else
                     return false;
             }
-            case 4:
-                {
-                Ciclo ciclo = (Ciclo)dato;
-                Cursor cciclo = db.query(true, "nota", new String[]{
-                        "detalle"},
-                        "detalle ='"+ ciclo.getCiclo()+ "'", null,null,null, null, null);
+            case 4: {
+                Ciclo ciclo = (Ciclo) dato;
+                Cursor cciclo = db.query(true, "nota", new String[]{"detalle"}, "detalle ='" + ciclo.getCiclo() + "'", null, null, null, null, null);
                 if (cciclo.moveToFirst())
-                    return  true;
+                    return true;
                 else
-                    return  false;
+                    return false;
             }
 
-            case 5:
-            {
+            case 5: {
 //verificar que exista actividad
                 Actividad actividad2 = (Actividad) dato;
                 String[] id = {actividad2.getCiclo()};
                 abrir();
                 Cursor c2 = db.query("actividad", null, "ciclo = ?", id, null, null,
                         null);
-                if(c2.moveToFirst()){
+                if (c2.moveToFirst()) {
 //Se encontro Actividad
                     return true;
                 }
@@ -315,22 +303,18 @@ public class ControlDBProyecto {
         }
     }
 
-    // fin de la verificacion de la integridad de los datos
 
-    // Inicio de llenado de Base de Datos
+    public String llenarBDProyecto() {
 
-    public String llenarBDProyecto(){
-        final String[] VAciclo = {"1","2"};
-        final String[] VAdetalle = {"actividad1","oactividad2"};
-        final String[] VAactividad = {"Evaluacion","Capacitacion"};
+        final String[] VAciclo = {"12017", "22017"};
+        final String[] VAdetalle = {"actividad 1", "actividad 2"};
+        final String[] VAactividad = {"parcial intro", "parcial repe"};
 
+        final String[] VCciclo = {"12017", "22017"};
+        final String[] VAdetallec = {"12017", "22017"};
 
-        final String[] VCciclo = {"12016","12016","22016","22016","22016","12016","22016"};
-        final String[] VCdescripcion = {"parcial MAT115"," parcial PRN115"};
-
-        final String [] VHciclo = { "12016", "22016"};
-        final String [] VHfecha = { "madre", "padre"};
-
+        final String[] VHciclo = {"12017", "22107"};
+        final String[] VHfecha = {"2/03", "3/08"};
 
         abrir();
         db.execSQL("DELETE FROM actividad");
@@ -338,28 +322,31 @@ public class ControlDBProyecto {
         db.execSQL("DELETE FROM horarioNo");
 
         Actividad actividad = new Actividad();
-
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 3; i++) {
             actividad.setCiclo(VAciclo[i]);
             actividad.setDetalle(VAdetalle[i]);
             actividad.setActividad(VAactividad[i]);
             insertar(actividad);
         }
+
+        Ciclo ciclo = new Ciclo();
+        for (int i = 0; i < 2; i++) {
+            ciclo.setCiclo(VAciclo[i]);
+            ciclo.setDetalle(VAdetalle[i]);
+
+            insertar(ciclo);
+        }
+
         HorarioNo horarioNo = new HorarioNo();
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 2; i++) {
 
             horarioNo.setCiclo(VHciclo[i]);
             horarioNo.setFecha(VHfecha[i]);
             insertar(horarioNo);
         }
-
-
-        Ciclo ciclo = new Ciclo();
-        for (int i=0; i<7;i++){
-
-        }
-
         cerrar();
-        return "Guardo Correctamente";
+        return "Guardado Correctamente";
+
     }
-        }
+
+}
