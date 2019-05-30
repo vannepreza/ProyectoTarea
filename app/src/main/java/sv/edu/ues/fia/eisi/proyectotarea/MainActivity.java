4ca81a19ca342400f388a7c6a9ce728dc8db46a4
Sofia Corpeño <hc14030@ues.edu.sv>
2019-05-30 12:00:59 -0600

    arreglando main

--- app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/MainActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/MainActivity.java
@@ -11,9 +11,9 @@ import android.widget.Toast;
 import sv.edu.ues.fia.eisi.proyectotarea.baseDatos.ControlDBProyecto;
 
 public class MainActivity extends ListActivity {
-    String[] menu={"Tabla Actividad","Tabla Ciclo","Tabla Horario No", "Tabla Tipo Actividad","Tabla Local","Tabla Horario","Tabla Disponiblidad Horario","Tabla Disponibiliad Ciclo","Tabla Tipo Local", "LLenar Base de Datos"};
+    String[] menu={"Tabla Actividad","Tabla Ciclo","Tabla Horario No", "Tabla Tipo Actividad","Docente","Solicitud","Carga Docente", "Cargo","Tabla Local","Tabla Horario","Tabla Disponiblidad Horario","Tabla Disponibiliad Ciclo","Tabla Tipo Local", "LLenar Base de Datos"};
             String[]
-    activities={"ActividadMenuActivity","CicloMenuActivity","HorarioNoMenuActivity","TipoAMenuActivity","LocalMenuActivity","HorarioMenuActivity","DispHorarioMenuActivity","DispCicloMenuActivity","TipoLocalMenuActivity"};
+    activities={"ActividadMenuActivity","CicloMenuActivity","HorarioNoMenuActivity","TipoAMenuActivity","DocenteMenuActivity","SolicitudMenuActivity","CargaDocenteMenuActivity","CargoMenuActivity","LocalMenuActivity","HorarioMenuActivity","DispHorarioMenuActivity","DispCicloMenuActivity","TipoLocalMenuActivity"};
 
     ControlDBProyecto BDhelper;
 
@@ -27,7 +27,7 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onListItemClick(ListView l,View v,int position,long id){
         super.onListItemClick(l, v, position, id);
-        if(position!=9){
+        if(position!=13){
             String nombreValue=activities[position];
             try{
                 Class<?>
