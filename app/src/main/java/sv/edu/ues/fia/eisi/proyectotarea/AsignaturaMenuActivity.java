fd0c3c2717600867ecebf4c63bdcc7b0815353f0
Karla <karabrego@hotmail.com>
2019-06-02 17:21:09 -0600

    Menus Activity

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/AsignaturaMenuActivity.java
@@ -0,0 +1,35 @@
+package sv.edu.ues.fia.eisi.proyectotarea;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class AsignaturaMenuActivity extends ListActivity {
+    String[] menu={"Insertar Registro"};
+    String[] activities={"AsignaturaInsertarActivity"};
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.parseColor("#B2EBF2"));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+        String nombreValue=activities[position];
+        l.getChildAt(position).setBackgroundColor(Color.parseColor("#E0F7FA"));
+        try{
+            Class<?> clase=Class.forName("sv.edu.ues.fia.eisi.locales."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/PensumMenuActivity.java
@@ -0,0 +1,36 @@
+package sv.edu.ues.fia.eisi.proyectotarea;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class PensumMenuActivity extends ListActivity {
+    String[] menu={"Insertar Registro","Eliminar Registro","Consultar Registro", "Actualizar Registro"};
+    String[] activities={"PensumInsertarActivity","PensumEliminarActivity","PensumConsultarActivity", "PensumActualizarActivity"};
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.parseColor("#B2EBF2"));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+        String nombreValue=activities[position];
+        l.getChildAt(position).setBackgroundColor(Color.parseColor("#E0F7FA"));
+        try{
+            Class<?> clase=Class.forName("sv.edu.ues.fia.eisi.locales."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
