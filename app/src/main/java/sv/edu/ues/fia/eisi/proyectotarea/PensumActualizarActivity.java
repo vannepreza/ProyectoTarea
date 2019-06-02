76cee3443baffbcd6bbf455459bcad1023408c90
Karla <karabrego@hotmail.com>
2019-06-02 11:23:07 -0600

    Pensum

--- app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/PensumActualizarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/PensumActualizarActivity.java
@@ -1,13 +1,52 @@
 package sv.edu.ues.fia.eisi.proyectotarea;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class PensumActualizarActivity extends AppCompatActivity {
+import sv.edu.ues.fia.eisi.proyectotarea.baseDatos.ControlDBProyecto;
+import sv.edu.ues.fia.eisi.proyectotarea.modelos.Pensum;
+
+public class PensumActualizarActivity extends Activity {
+
+
+    ControlDBProyecto helper;
+    private EditText editId_Pensum;
+    private EditText  editNombre_Materia;
+    private EditText  editId_Carrera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pensum_actualizar);
+        helper = new ControlDBProyecto(this);
+
+        editId_Pensum = (EditText) findViewById(R.id.editId_Pensum);
+        editNombre_Materia = (EditText) findViewById(R.id.editNombre_Materia);
+        editId_Carrera= (EditText) findViewById(R.id.editId_Carrera);
+
+
+    }
+
+    public void actualizarPensum(View v) {
+        Pensum pensum = new Pensum();
+        pensum.setId_pensum(editId_Pensum.getText().toString());
+        pensum.setNombre_materia(editNombre_Materia.getText().toString());
+        pensum.setId_carrera(editId_Carrera.getText().toString());
+        helper.abrir();
+        String estado = helper.actualizarPensum(pensum);
+        helper.cerrar();
+        Toast.makeText(this, estado, Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarTexto(View v) {
+
+        editId_Pensum.setText("");
+        editNombre_Materia.setText("");
+        editId_Carrera.setText("");
+
     }
 }
+
--- app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/PensumConsultarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/proyectotarea/PensumConsultarActivity.java
@@ -1,13 +1,52 @@
 package sv.edu.ues.fia.eisi.proyectotarea;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class PensumConsultarActivity extends AppCompatActivity {
+import sv.edu.ues.fia.eisi.proyectotarea.baseDatos.ControlDBProyecto;
+import sv.edu.ues.fia.eisi.proyectotarea.modelos.Pensum;
 
+public class PensumConsultarActivity extends Activity {
+
+    ControlDBProyecto helper;
+    EditText editId_Pensum;
+    EditText editNombre_Materia;
+    EditText editId_Carrera;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pensum_consultar);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); setContentView(R.layout.activity_pensum_consultar);
+        helper = new ControlDBProyecto (this);
+
+        editId_Pensum = (EditText) findViewById(R.id.editId_Pensum);
+        editNombre_Materia = (EditText) findViewById(R.id.editNombre_Materia);
+        editId_Carrera = (EditText) findViewById(R.id.editId_Carrera);
+    }
+
+    public void  ConsultarPensum(View v)
+    {
+        helper.abrir();
+        Pensum pensum = helper.ConsultarPensum(editId_Pensum.getText().toString());
+        helper.cerrar();
+        if(pensum == null)
+            Toast.makeText(this, "Pensum con ID " + editId_Carrera.getText().toString() +
+                    " no encontrada", Toast.LENGTH_LONG).show();
+        else{
+
+            editNombre_Materia.setText(pensum.getNombre_materia());
+            editId_Carrera.setText(pensum.getId_carrera());
+
+        }
+    }
+    public void limpiarTexto(View v){
+
+        editId_Pensum.setText("");
+        editNombre_Materia.setText("");
+        editId_Carrera.setText("");
+
     }
 }
